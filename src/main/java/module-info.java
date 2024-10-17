56cd7ebee9c74cbe41f4a486a65eef199c0e11ae
Lucas Lowe <lowel9295@gmail.com>
2024-10-17 14:58:08 -0400

    Remove sample code

--- src/main/java/module-info.java
+++
@@ -1,9 +0,0 @@
-module bsu.cs222.finalprojectaiimagegenerator {
-    requires javafx.controls;
-    requires javafx.fxml;
-    requires java.desktop;
-
-
-    opens bsu.cs222.finalprojectaiimagegenerator to javafx.fxml;
-    exports bsu.cs222.finalprojectaiimagegenerator;
-}
