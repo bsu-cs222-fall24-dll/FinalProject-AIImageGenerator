ad24b1be916ab39173995d48ef67fb791af321f0
Lucas Lowe <lowel9295@gmail.com>
2024-10-20 14:41:52 -0400

    Add `PromptBuilder` class - Added `PromptBuilder` class, takes in a `Characteristics` class instance   and builds a prompt for the image generation api. - Added unit tests for the `PromptBuilder` class

---
+++ src/main/java/bsu/edu/cs222/PromptBuilder.java
@@ -0,0 +1,61 @@
+package bsu.edu.cs222;
+
+public class PromptBuilder {
+    private final Characteristics characteristics;
+
+    public PromptBuilder(Characteristics characteristics) {
+        this.characteristics = characteristics;
+    }
+
+    public String buildPrompt() {
+        StringBuilder prompt = new StringBuilder("A ");
+
+        if (characteristics.getAge() != null) {
+            prompt.append(characteristics.getAge()).append(" year old ");
+        }
+
+        if (characteristics.getSex() != null) {
+            prompt.append(characteristics.getSex()).append(" ");
+        }
+
+        if (characteristics.getRace() != null) {
+            prompt.append(characteristics.getRace()).append(", ");
+        }
+
+        if (characteristics.getHairColor() != null) {
+            prompt.append("with ").append(characteristics.getHairColor()).append(" hair ");
+        }
+
+        if (characteristics.getEyeColor() != null) {
+            if (characteristics.getHairColor() != null) {
+                prompt.append("and ");
+            } else {
+                prompt.append("with ");
+            }
+            prompt.append(characteristics.getEyeColor()).append(" eyes, ");
+        }
+
+        if (characteristics.getBodyStyle() != null) {
+            prompt.append("having a ").append(characteristics.getBodyStyle()).append(" body style.");
+        } else {
+            prompt.append("with an undefined body style.");
+        }
+
+        if (characteristics.isGameCharacter()) {
+            prompt.append(" The character is depicted in a ");
+            if (characteristics.getArtStyle() != null) {
+                prompt.append(characteristics.getArtStyle()).append(" art style, ");
+            }
+            if (characteristics.getCharacterType() != null) {
+                prompt.append("and is a ").append(characteristics.getCharacterType()).append(" ");
+            }
+            if (characteristics.getGameType() != null) {
+                prompt.append("in a ").append(characteristics.getGameType()).append(" game.");
+            } else {
+                prompt.append("in an unspecified type of game.");
+            }
+        }
+
+        return prompt.toString();
+    }
+}
