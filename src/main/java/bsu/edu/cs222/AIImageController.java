431120ffdfa3056a3a441300f44328f243a8be8a
Lucas Lowe <lowel9295@gmail.com>
2024-10-23 14:35:48 -0400

    Fix small indexing issue

--- src/main/java/bsu/edu/cs222/AIImageController.java
+++ src/main/java/bsu/edu/cs222/AIImageController.java
@@ -23,7 +23,7 @@ public class AIImageController {
     private Characteristics getCharacteristicsFromArgs(boolean isGameCharacter, String[] args) {
         checkForEmptyFields(args);
 
-        if (isGameCharacter) return new Characteristics(args[0], args[1], args[2], args[3], args[4], args[5], args[6], args[7], args[8], args[10], args[11]);
+        if (isGameCharacter) return new Characteristics(args[0], args[1], args[2], args[3], args[4], args[5], args[6], args[7], args[8], args[9], args[10]);
         else return new Characteristics(args[0], args[1], args[2], args[3], args[4], args[5]);
     }
 
