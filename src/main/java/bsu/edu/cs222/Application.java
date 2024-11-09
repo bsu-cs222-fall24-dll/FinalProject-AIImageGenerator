e2a2e48d6ff54bee3049b5f9f5828c7340dba111
1vke <lowel9295@gmail.com>
2024-11-09 18:28:26 -0500

    Add `SceneController` class, update `Application` to implement the new class

--- src/main/java/bsu/edu/cs222/Application.java
+++ src/main/java/bsu/edu/cs222/Application.java
@@ -1,25 +1,14 @@
 package bsu.edu.cs222;
 
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
 import javafx.stage.Stage;
 
-import java.io.IOException;
-import java.util.Objects;
-
 public class Application extends javafx.application.Application {
-    Stage primaryStage;
+    SceneController sceneController = new SceneController();
 
     @Override
-    public void start(Stage stage) throws IOException {
-        primaryStage = stage;
-        Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getClassLoader().getResource("generator-view.fxml")));
-
-        Scene scene = new Scene(root);
-        primaryStage.setScene(scene);
-
-        primaryStage.show();
+    public void start(Stage stage) {
+        sceneController.setStage(stage);
+        sceneController.switchSceneToManagerView();
     }
 
     public static void main(String[] args) {
---
+++ src/main/java/bsu/edu/cs222/SceneController.java
@@ -0,0 +1,77 @@
+package bsu.edu.cs222;
+
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+public class SceneController {
+    private Stage stage;
+
+    final private String GENERATOR_VIEW = "generator-view.fxml";
+
+    void setStage(Stage stage) {
+        this.stage = stage;
+    }
+
+    FXMLLoader getLoader(String sceneName) {
+        return new FXMLLoader(getClass().getResource("/" + sceneName));
+    }
+
+    void setAndShowSceneFromParent(Parent root) throws IOException {
+        Scene scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public void switchSceneToManagerView() {
+        try {
+            String MANAGER_VIEW = "manager-view.fxml";
+            FXMLLoader loader = getLoader(MANAGER_VIEW);
+            Parent root = loader.load();
+
+            ManagerViewController controller = loader.getController();
+            controller.setSceneController(this);
+
+            setAndShowSceneFromParent(root);
+        } catch (IOException e) {
+            throw new RuntimeException("There was an error whilst trying to switch" +
+                    " the scene to the manager view!", e);
+        }
+    }
+
+    public void switchToGeneratorView() {
+        try {
+            FXMLLoader loader = getLoader(GENERATOR_VIEW);
+            Parent root = loader.load();
+
+            GeneratorViewController controller = loader.getController();
+            controller.setSceneController(this);
+
+            setAndShowSceneFromParent(root);
+        } catch (IOException e) {
+            throw new RuntimeException("There was an error whilst trying to switch" +
+                    " the scene to the generator view!", e);
+        }
+    }
+
+    public void switchToGeneratorView(InputStream imageStream, Characteristics characteristics) {
+        try {
+            FXMLLoader loader = getLoader(GENERATOR_VIEW);
+            Parent root = loader.load();
+
+            GeneratorViewController controller = loader.getController();
+            controller.setSceneController(this);
+            controller.setCharacteristics(characteristics);
+            controller.setImage(imageStream);
+
+            setAndShowSceneFromParent(root);
+        } catch (IOException e) {
+            throw new RuntimeException("There was an error whilst trying to switch" +
+                    " the scene to the generator view with initial data!", e);
+        }
+    }
+}
