1948013ee7c638734a5b1db4e153b1abeb65b784
Josh <joshua.smith2@bsu.edu>
2024-11-12 04:23:23 -0500

    Add tests in ImageFetcherTest to test for spaces and empty inputs

--- src/test/java/bsu/edu/cs222/ImageFetcherTest.java
+++ src/test/java/bsu/edu/cs222/ImageFetcherTest.java
@@ -9,11 +9,29 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 class ImageFetcherTest {
 
     @Test
-    void testCreateUrl() throws IOException, URISyntaxException {
+    void testCreateUnderscoreUrl() throws IOException, URISyntaxException {
         ImageFetcher fetcher = new ImageFetcher();
         String searchInput = "red_xbox";
         URL url = fetcher.createUrl(searchInput);
         String expectedUrl = "https://image.pollinations.ai/prompt/red_xbox";
+        assertEquals(expectedUrl, url.toString());
+    }
+
+    @Test
+    void testCreateSpaceUrl() throws IOException, URISyntaxException {
+        ImageFetcher fetcher = new ImageFetcher();
+        String searchInput = "red xbox";
+        URL url = fetcher.createUrl(searchInput);
+        String expectedUrl = "https://image.pollinations.ai/prompt/red+xbox";
+        assertEquals(expectedUrl, url.toString());
+    }
+    
+    @Test
+    void testCreateEmptyUrl() throws IOException, URISyntaxException {
+        ImageFetcher fetcher = new ImageFetcher();
+        String searchInput = "";
+        URL url = fetcher.createUrl(searchInput);
+        String expectedUrl = "https://image.pollinations.ai/prompt/";
         assertEquals(expectedUrl, url.toString());
     }
 }
